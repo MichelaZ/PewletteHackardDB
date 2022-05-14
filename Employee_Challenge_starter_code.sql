0aba14d59f80ed1bc68682bf1bd80c2a25b7cdf2
michelaziemer-YzKkEV <michelaziemer@gmail.com>
2022-05-14 14:55:48 -0500

    HW7 V4 Deliverable 1

--- Employee_Challenge_starter_code.sql
+++ Employee_Challenge_starter_code.sql
@@ -1,10 +1,90 @@
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--The Number of Retiring Employees by Title
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no, to_date DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+Select * from retirement_titles 
+
+SELECT DISTINCT ON (ce.emp_no) ce.emp_no,
+ce.first_name,
+ce.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM current_emp AS ce
+	INNER JOIN titles AS t
+		ON (ce.emp_no = t.emp_no)
+ORDER BY emp_no, to_date DESC;
+
+SELECT e.emp_no,
+e.first_name,
+ce.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY emp_no, to_date DESC;
+
+-- using current_emp
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM current_emp AS ce
+	INNER JOIN titles AS t
+		ON (ce.emp_no = t.emp_no)
+	INNER JOIN employees as e
+		On (ce.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY emp_no, to_date DESC;
+
+--Current_titles
+SELECT DISTINCT ON (ce.emp_no) ce.emp_no,
+ce.first_name,
+ce.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO unique_titles
+FROM current_emp AS ce
+	INNER JOIN titles AS t
+		ON (ce.emp_no = t.emp_no)
+	INNER JOIN employees as e
+		On (ce.emp_no = e.emp_no)
+ORDER BY emp_no, to_date DESC;
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title,
+rt.from_date,
+rt.to_date
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+
+
+
+Select * from retirement_titles 
